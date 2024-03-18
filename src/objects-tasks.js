c7fbb7b931f0808152421e3bc9a11c4dbfa21ef5
Slava Manzhela <slavamanzh@gmail.com>
2024-03-18 23:48:46 +0300

    feat: add 2 more solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -128,8 +128,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arrSize = Object.values(lettersObject).flat().length;
+  const letters = new Array(arrSize).fill('');
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((index) => {
+      letters[index] = key;
+    });
+  });
+  return letters.join('');
 }
 
 /**
@@ -146,8 +153,18 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let cash = 0;
+  const ticketPrice = 25;
+  for (let i = 0; i < queue.length; i += 1) {
+    const change = queue[i] - ticketPrice;
+    if (change > cash) {
+      return false;
+    }
+    cash -= change;
+    cash += ticketPrice;
+  }
+  return true;
 }
 
 /**
